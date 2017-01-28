7e2f3639198dd4a5e87718cdf30c224c0b95e5b0
Ian Howell <ian.howell0@gmail.com>
2017-01-28 00:33:11 -0600

    If you can't take the heap, stay outta the kitchen

---
+++ heap.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+from random import randrange
+
+class Heap(object):
+    def __init__(self):
+        self.heap = []
+        self.__size = 0
+
+    def get(self):
+        ret_val = None
+        if self.__size > 0:
+            self.__size -= 1
+            ret_val = self.heap[0]
+            self.heap[0] = self.heap.pop()
+            self.heapify(0)
+        return ret_val
+
+    def put(self, x):
+        self.__size += 1
+        self.heap.append(x)
+
+        i = self.__size - 1
+        parent = (i - 1) / 2
+        while (i > 0 and self.heap[i] < self.heap[parent]):
+            self.heap[i], self.heap[parent] = self.heap[parent], self.heap[i]
+            i = (i - 1) / 2
+            parent = (i - 1) / 2
+
+    def size(self):
+        return self.__size
+
+    def heapify(self, i):
+        smallest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+        if left < self.__size and self.heap[left] < self.heap[smallest]:
+            smallest = left
+
+        if right < self.__size and self.heap[right] < self.heap[smallest]:
+            smallest = right
+
+        if smallest != i:
+            self.heap[i], self.heap[smallest] = self.heap[smallest], self.heap[i]
+            self.heapify(smallest)
+
+    def __repr__(self):
+        return '<Heap object>'
+
+    def __str__(self):
+        ret_val = 'Size = {0}\n'.format(self.__size)
+        ret_val += 'Contents: [ '
+        for x in self.heap:
+            ret_val += str(x) + ' '
+        ret_val += ']'
+        return ret_val
+
+def main():
+    min_heap = Heap()
+
+    for i in range(10):
+        min_heap.put(randrange(1,100))
+    print(min_heap)
+
+    first_item = min_heap.get()
+
+    print('Got {0} from the heap'.format(first_item))
+    print(min_heap)
+
+if __name__ == '__main__':
+    main()
