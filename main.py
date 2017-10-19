da8aafb0b8efcb616af659e387385a8fa7d1151b
Ian Howell <ian.howell0@gmail.com>
2017-10-19 11:33:20 -0500

    Test heap sort and clean up main

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import heap
 
 
 def main():
+    test_heap_creation()
+    test_heap_sort()
+
+
+def test_heap_creation():
     min_heap = heap.Heap()
 
     for i in range(10):
@@ -17,6 +22,17 @@ def main():
 
     print('Got {0} from the heap'.format(first_item))
     print(min_heap)
+
+
+def test_heap_sort():
+    ARR_SIZE = 20
+    random_arr = [None] * ARR_SIZE
+    for i in range(ARR_SIZE):
+        random_arr[i] = randrange(1, 100)
+
+    print("Array before heap sort: {}".format(random_arr))
+    heap.heap_sort(random_arr)
+    print("Array after heap sort: {}".format(random_arr))
 
 
 if __name__ == "__main__":
